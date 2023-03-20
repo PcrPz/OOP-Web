e46dc1d91a279e64dc2155af793feff52ed200f9
Passakorn Polsan <65010833@kmitl.ac.th>
2023-03-20 20:58:49 +0700

    <booking> add class
    add Class : FavouriteCar, Booking,BookHistory,Total

---
+++ BookingHistory.py
@@ -0,0 +1,4 @@
+class BookHistory:
+    def __init__(self,history_car_booking,history_payment):
+        self.__history_car_booking = history_car_booking
+        self.__history_payment = history_payment
