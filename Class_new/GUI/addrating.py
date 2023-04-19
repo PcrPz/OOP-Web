fce9eb8d369b5860b4ed0160a9ac4091dce538d1
PcrPz <Pacharaeiamsongkram@gmail.com>
2023-04-19 13:10:37 +0700

    Feat: AddRatingGUI

--- Class_new/GUI/addrating.py
+++ Class_new/GUI/addrating.py
@@ -1,2 +1,10 @@
 from tkinter import *
 from tkinter.font import *
+
+class AddRating:
+    def __init__(self,token):
+        self.__add_rating = Tk()
+        #font
+        self.__title_font = Font(family="Kanit", weight="bold", size=20)
+        self.__normal_font = Font(family="Kanit", weight="normal", size=16)
+        self.__text_font = Font(family="Kanit", weight="normal", size=12) 
